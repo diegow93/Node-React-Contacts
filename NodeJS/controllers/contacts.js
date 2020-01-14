4d2302fa16fb5a5a3ba58e09cc6eb33fdfbf9842
Diego <none>
2020-01-14 19:20:04 -0300

    Add and remove contacts working.

--- NodeJS/controllers/contacts.js
+++ NodeJS/controllers/contacts.js
@@ -1,16 +1,36 @@
-const test = (req, res, db) => {
+const addContact = (req, res, db) => {
+    const { firstName, lastName, address, phoneNumber } = req.body
     db.query(
         'INSERT INTO contacts (firstname, lastname, address, phonenumber) VALUES ($1, $2, $3, $4) RETURNING *',
-        ['nombre2', 'apellido', 'aveida italia', '123456'],
-        (err, result) => {
-            if (err) {
-                res.send(err)
+        [firstName, lastName, address, phoneNumber],
+        (error, result) => {
+            if (error) {
+                res.send(error)
+            } else {
+                res.send(result.rows[0])
             }
-            res.send(result.rows[0])
+        }
+    )
+}
+
+const removeContact = (req, res, db) => {
+    const { id } = req.body
+    db.query(
+        'DELETE FROM contacts WHERE id = $1 RETURNING *',
+        [id],
+        (error, result) => {
+            if (error) {
+                res.send(error)
+            } else if (result.rows[0]) {
+                res.send(result.rows[0])
+            } else {
+                res.send(`There is no user with ID: ${id}`)
+            }
         }
     )
 }
 
 module.exports = {
-    test
+    addContact,
+    removeContact
 }
--- NodeJS/server.js
+++ NodeJS/server.js
@@ -3,33 +3,34 @@ const express = require('express')
 // External config file.
 require('dotenv').config()
 
-// Middleware to be used.
-const helmet = require('helmet')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-
-// Controllers.
-const contactsController = require('./controllers/contacts')
-
 // Express app.
 const app = express()
 
 // Database.
 const db = require('./db/index')
 
+// Middleware to be used.
+const helmet = require('helmet')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
 // Use un every request.
 app.use(helmet())
 app.use(bodyParser.json())
 app.use(morgan('combined'))
 
+// Controllers.
+const contactsController = require('./controllers/contacts')
+
 // Routes.
-app.get('/', (req, res) => res.send('Hello world!')) // Verify this works
-app.get('/all', (req, res) => contactsController.METHOD(req, res, db)) // change METHOD for
-// actual method
-app.get('/contacts', (req, res) => contactsController.test(req, res, db)) // TESTING
-app.post('/contacts', (req, res) => contactsController.METHOD(req, res, db))
+app.get('/', (req, res) => res.send('Hello world!'))
+app.get('/all', (req, res) => contactsController.METHOD(req, res, db))
+app.get('/contacts', (req, res) => contactsController.METHOD(req, res, db))
+app.post('/contacts', (req, res) => contactsController.addContact(req, res, db))
 app.put('/contacts', (req, res) => contactsController.METHOD(req, res, db))
-app.delete('/contacts', (req, res) => contactsController.METHOD(req, res, db))
+app.delete('/contacts', (req, res) =>
+    contactsController.removeContact(req, res, db)
+)
 
 // Server connection.
 app.listen(process.env.PORT || 3000, () => {
